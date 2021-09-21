7c411c08f9b71e3386bdce6bc8c1c52430ba5165
Daniel Fireman <danielfireman@gmail.com>
2021-09-21 10:59:20 -0300

    agora falando proto

--- go.mod
+++ go.mod
@@ -1,13 +1,14 @@
-module packager
+module empacotador
 
 go 1.14
 
 require (
 	github.com/dadosjusbr/coletores v0.0.0-20210827150800-92e4a6a7f133
-	github.com/dadosjusbr/proto v0.0.0-20210830014001-8114f7d4c697
+	github.com/dadosjusbr/proto v0.0.0-20210921134625-bc3fa5eb0fd7
 	github.com/frictionlessdata/datapackage-go v0.0.0-20210810130302-98883c9586e9
 	github.com/frictionlessdata/tableschema-go v1.1.3 // indirect
 	github.com/gocarina/gocsv v0.0.0-20210516172204-ca9e8a8ddea8
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/check.v1 v1.0.0-20200902074654-038fdea0a05b // indirect
 )
--- main.go
+++ main.go
@@ -14,7 +14,10 @@ import (
 	"github.com/dadosjusbr/coletores/status"
 	"github.com/dadosjusbr/proto/coleta"
 	"github.com/dadosjusbr/proto/csv"
+	"github.com/dadosjusbr/proto/pipeline"
 	"github.com/frictionlessdata/datapackage-go/datapackage"
+	"github.com/gocarina/gocsv"
+	"google.golang.org/protobuf/proto"
 )
 
 const (
@@ -29,12 +32,12 @@ func main() {
 	if outputPath == "" {
 		outputPath = "./"
 	}
-	var er ExecutionResult
+	var er pipeline.ResultadoExecucao
 	erIN, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		status.ExitFromError(status.NewError(4, fmt.Errorf("error reading crawling result: %q", err)))
 	}
-	if err = json.Unmarshal(erIN, &er.Rc); err != nil {
+	if err = proto.Unmarshal(erIN, er.Rc); err != nil {
 		status.ExitFromError(status.NewError(5, fmt.Errorf("error unmarshaling crawling resul from STDIN: %q", err)))
 	}
 
@@ -59,13 +62,13 @@ func main() {
 	}
 
 	// Creating contracheque csv
-	if err := ToCSVFile(csvRc.Folha.ContraCheque, folhaFileName); err != nil {
+	if err := toCSVFile(csvRc.Folha.ContraCheque, folhaFileName); err != nil {
 		err = status.NewError(status.InvalidParameters, fmt.Errorf("error creating Folha de pagamento CSV:%q", err))
 		status.ExitFromError(err)
 	}
 
 	// Creating remuneracao csv
-	if err := ToCSVFile(csvRc.Remuneracoes.Remuneracao, remuneracaoFileName); err != nil {
+	if err := toCSVFile(csvRc.Remuneracoes.Remuneracao, remuneracaoFileName); err != nil {
 		err = status.NewError(status.InvalidParameters, fmt.Errorf("error creating Remuneração CSV:%q", err))
 		status.ExitFromError(err)
 	}
@@ -92,13 +95,15 @@ func main() {
 	// Packing CSV and package descriptor.
 	zipName := filepath.Join(outputPath, fmt.Sprintf("%s-%d-%d.zip", er.Rc.Coleta.Orgao, er.Rc.Coleta.Ano, er.Rc.Coleta.Mes))
 	if err := pkg.Zip(zipName); err != nil {
-		err = status.NewError(status.SystemError, fmt.Errorf("Error zipping datapackage (%s):%q", zipName, err))
+		err = status.NewError(status.SystemError, fmt.Errorf("error zipping datapackage (%s):%q", zipName, err))
 		status.ExitFromError(err)
 	}
 
 	// Sending results.
-	er.Pr = PackagingResult{Package: zipName}
-	b, err := json.MarshalIndent(er, "", "  ")
+	er.Pr = &pipeline.ResultadoEmpacotamento{
+		Pacote: zipName,
+	}
+	b, err := proto.Marshal(&er)
 	if err != nil {
 		err = status.NewError(status.Unknown, fmt.Errorf("error marshalling packaging result (%s):%q", zipName, err))
 		status.ExitFromError(err)
@@ -106,7 +111,7 @@ func main() {
 	fmt.Println(string(b))
 }
 
-func coletaToCSV(rc coleta.ResultadoColeta) csv.ResultadoColeta_CSV {
+func coletaToCSV(rc *coleta.ResultadoColeta) csv.ResultadoColeta_CSV {
 	var coleta csv.Coleta_CSV
 	var remuneracoes csv.Remuneracoes_CSV
 	var folha csv.FolhaDePagamento_CSV
@@ -149,4 +154,14 @@ func buildPacoteCSV(s string) [][]string {
 	b = append(b, strings.Split(a[0], ","))
 	b = append(b, strings.Split(a[1], ","))
 	return b
+}
+
+// ToCSVFile dumps the payroll into a file using the CSV format.
+func toCSVFile(in interface{}, path string) error {
+	f, err := os.Create(path)
+	if err != nil {
+		return fmt.Errorf("error creating CSV file(%s):%q", path, err)
+	}
+	defer f.Close()
+	return gocsv.MarshalFile(in, f)
 }
