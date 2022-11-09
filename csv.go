de52845fd29ee7031efd717ad70cf18f350852df
duardoqueiroz <eduardoldq1@gmail.com>
2022-11-09 03:44:26 -0300

    feat: create remunerations zip

---
+++ csv.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gocarina/gocsv"
+)
+
+type Remuneracao struct {
+	Ano                      int32   `csv:"ano" tableheader:"ano"`
+	Mes                      int32   `csv:"mes" tableheader:"mes"`
+	Orgao                    string  `csv:"orgao" tableheader:"orgao"`
+	Nome                     string  `csv:"nome" tableheader:"nome"`
+	Matricula                string  `csv:"matricula" tableheader:"matricula"`
+	Cargo                    string  `csv:"cargo" tableheader:"cargo"`
+	Lotacao                  string  `csv:"lotacao" tableheader:"lotacao"`
+	DetalhamentoContracheque string  `csv:"detalhamento_contracheque" tableheader:"detalhamento_contracheque"`
+	CategoriaContracheque    string  `csv:"categoria_contracheque" tableheader:"categoria_contracheque"`
+	Valor                    float64 `csv:"valor" tableheader:"valor"`
+}
+
+func toCSVFile(in interface{}, path string) error {
+	f, err := os.Create(path)
+	if err != nil {
+		return fmt.Errorf("error creating CSV file(%s):%q", path, err)
+	}
+	defer f.Close()
+	return gocsv.MarshalFile(in, f)
+}
--- go.mod
+++ go.mod
@@ -6,15 +6,17 @@ require (
 	github.com/dadosjusbr/coletores v0.0.0-20210928113650-4c03e4fa3b74
 	github.com/dadosjusbr/datapackage v0.0.0-20221101144952-610ed98d8bc2
 	github.com/dadosjusbr/proto v0.0.0-20220304160723-47e93e9d97b5
+	github.com/gocarina/gocsv v0.0.0-20220927221512-ad3251f9fa25
+	golang.org/x/text v0.4.0
 	google.golang.org/protobuf v1.28.1
 )
 
 require (
 	github.com/frictionlessdata/datapackage-go v1.0.4 // indirect
 	github.com/frictionlessdata/tableschema-go v1.1.4-0.20220401172006-6cc5f3b2411c // indirect
-	github.com/gocarina/gocsv v0.0.0-20220927221512-ad3251f9fa25 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/rogpeppe/go-internal v1.8.1 // indirect
 	github.com/santhosh-tekuri/jsonschema v1.2.4 // indirect
 	github.com/satori/go.uuid v1.2.0 // indirect
+	golang.org/x/exp v0.0.0-20221108223516-5d533826c662
 )
