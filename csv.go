f347dd5a657d099d45987b89d4685ba854338418
Joellensilva <joellen.absilva@gmail.com>
2024-03-21 15:59:01 -0300

    atualizando datapackage e modificando o separador de decimal e de colunas

--- csv.go
+++ csv.go
@@ -1,23 +1,25 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"os"
 
+	"github.com/dadosjusbr/datapackage"
 	"github.com/gocarina/gocsv"
 )
 
 type Remuneracao struct {
-	Ano                      int32   `csv:"ano" tableheader:"ano"`
-	Mes                      int32   `csv:"mes" tableheader:"mes"`
-	Orgao                    string  `csv:"orgao" tableheader:"orgao"`
-	Nome                     string  `csv:"nome" tableheader:"nome"`
-	Matricula                string  `csv:"matricula" tableheader:"matricula"`
-	Cargo                    string  `csv:"cargo" tableheader:"cargo"`
-	Lotacao                  string  `csv:"lotacao" tableheader:"lotacao"`
-	DetalhamentoContracheque string  `csv:"detalhamento_contracheque" tableheader:"detalhamento_contracheque"`
-	CategoriaContracheque    string  `csv:"categoria_contracheque" tableheader:"categoria_contracheque"`
-	Valor                    float64 `csv:"valor" tableheader:"valor"`
+	Ano                      int32                     `csv:"ano" tableheader:"ano"`
+	Mes                      int32                     `csv:"mes" tableheader:"mes"`
+	Orgao                    string                    `csv:"orgao" tableheader:"orgao"`
+	Nome                     string                    `csv:"nome" tableheader:"nome"`
+	Matricula                string                    `csv:"matricula" tableheader:"matricula"`
+	Cargo                    string                    `csv:"cargo" tableheader:"cargo"`
+	Lotacao                  string                    `csv:"lotacao" tableheader:"lotacao"`
+	DetalhamentoContracheque string                    `csv:"detalhamento_contracheque" tableheader:"detalhamento_contracheque"`
+	CategoriaContracheque    string                    `csv:"categoria_contracheque" tableheader:"categoria_contracheque"`
+	Valor                    datapackage.CustomFloat32 `csv:"valor" tableheader:"valor"`
 }
 
 type Categoria struct {
@@ -32,5 +34,11 @@ func toCSVFile(in interface{}, path string) error {
 		return fmt.Errorf("error creating CSV file(%s):%q", path, err)
 	}
 	defer f.Close()
-	return gocsv.MarshalFile(in, f)
+
+	// Crie um novo escritor CSV com os separadores personalizados
+	csvWriter := csv.NewWriter(f)
+	csvWriter.Comma = ';'
+	csvWriter.UseCRLF = true // Para garantir que os fins de linha estejam no formato correto
+
+	return gocsv.MarshalCSV(in, csvWriter)
 }
